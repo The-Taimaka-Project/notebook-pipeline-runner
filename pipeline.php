1705d4e3e8cea473234311185a985bb729b38094
Kevin Monisit <monisitkevin@gmail.com>
2023-02-03 23:00:54 -0500

    Adding timeout and modularizing functions

--- pipeline.php
+++ pipeline.php
@@ -1,38 +1,44 @@
 <?php
 
-$history = './history.log';
-$state = './state.txt';
-$jupyter_notebook_path = './notebook.ipynb';
+enum PipelineState: String {
+    case Running = "Running";
+    case Completed = "Completed";
+    case Failed = "Failed";
+}
+
+$path_to_history_log = './history.log';
+$path_to_state_file = './state.txt';
+
+function logStatus($return_code, $output) {
+    global $path_to_history_log, $path_to_state_file;
+
+    $state = $return_code == 0 ? PipelineState::Completed : PipelineState::Failed;
+
+    file_put_contents($path_to_state_file, $state . date('Y-m-d H:i:s'), LOCK_EX);
+    file_put_contents($path_to_history_log, $state . date('Y-m-d H:i:s'), FILE_APPEND);
+
+    return array('status' => $state, 'output' => implode("\n", $output));
+}
 
 // add an argument to the function to pass the notebook name
 function runNotebook($notebook) {
-    global $history, $state, $jupyter_notebook_path;
+    global $path_to_history_log, $path_to_state_file;
     $command = 'jupyter nbconvert --to html ' . $notebook;
+    $timeout = 20 * 60; // in seconds
 
-    exec($command, $output, $returnValue);
-
-    if ($returnValue == 0) {
-        file_put_contents($state, 'Completed');
-        file_put_contents($history, 'Success: ' . date('Y-m-d H:i:s'), FILE_APPEND);
-
-        return array('status' => 'Success', 'output' => implode("\n", $output));
-    } else {
-        file_put_contents($state, 'Failed');
-        file_put_contents($history, 'Failed: ' . date('Y-m-d H:i:s'), FILE_APPEND);
-
-        return array('status' => 'Failed', 'output' => implode("\n", $output));
-    }
+    set_time_limit($timeout);
+    exec($command, $output, $return_code);
+    return logStatus($return_code, $output);
 }
 
 $notebooks = array('notebook1.ipynb', 'notebook2.ipynb', 'notebook3.ipynb');
-$lock_file = './pipeline.lock';
+$path_to_lock_file = './pipeline.lock';
 
-if (!file_exists($lock_file)) {
-    global $notebooks, $lock_file, $state;
+if(!file_exists($path_to_lock_file) && flock($path_to_lock_file, LOCK_EX | LOCK_NB)) {
+    global $notebooks, $path_to_lock_file, $path_to_state_file;
 
-    //put the date into the lock file
-    file_put_contents($lock_file, date('Y-m-d H:i:s'));
-    file_put_contents($state, 'Running');
+    file_put_contents($path_to_lock_file, date('Y-m-d H:i:s'));
+    file_put_contents($path_to_state_file, PipelineState::Running . date('Y-m-d H:i:s'), LOCK_EX);
 
     $result = array();
     $result = runPipeline();
@@ -42,10 +48,12 @@ if (!file_exists($lock_file)) {
         $result[$notebook] = $output;
     }
 
-    unlink($lock_file);
+    file_put_contents($path_to_state_file, PipelineState::Completed . date('Y-m-d H:i:s'), LOCK_EX);
 
     header('Content-Type: application/json');
     echo json_encode($result);
+
+    flock($path_to_lock_file, LOCK_UN);
 } else {
     // Return an error indicating that the pipeline is already running
     header('Content-Type: application/json');
